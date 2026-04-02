16340d321057ab602ec6a24ac2495e7a5d5bb887
Rahul Patil <rahulpatil6451@gmail.com>
2026-04-02 19:38:23 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+class Main {
+  public static void main(String[] args) {
+
+    // Variables
+       int a = 10;
+       System.out.println(a);
+       // Variable is used to store the value
+
+
+    // Data Types
+     //There are two types of Data types Primitive and nonPrimitive
+
+     //Primitive
+      //byte-1,boolean-1,short-2,char-2,int-4,float-4,long-8,double-8
+
+    //ex
+     int b =20;
+     System.out.println(b);
+
+     //NonPrimitive
+    //String,Array
+
+    String name = new String("Hello");
+    System.out.println(name);
+
+    //Array
+
+    int [] marks = {90,98,95};
+    System.out.println(marks[0]);
+
+    //2D Array
+
+    int [] [] student = {{51,52,53},{61,62,63}};
+    System.out.println(student[0][0]);
+    System.out.println(student[0][1]);
+    System.out.println(student[0][2]);
+    System.out.println(student[1][0]);
+    System.out.println(student[1][1]);
+    System.out.println(student[1 ][2]);
+
+
+
+  }
+}
